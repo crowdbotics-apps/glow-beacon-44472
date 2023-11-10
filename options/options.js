c2ddbacb510a91dd1838f2dae2780be355d0c8b8
Crowdbotics <team@crowdbotics.com>
2023-11-10 18:14:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "glow-beacon-44472",
+  url: "https://glow-beacon-44472.botics.co",
+  api: "https://glow-beacon-44472.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
